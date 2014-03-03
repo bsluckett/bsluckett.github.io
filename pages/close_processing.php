01232337387ebcec5c53949019b7156ac4a5551e
Breanna Luckett <bluckett@cisco.com>
2014-03-03 16:42:29 -0500

    added ET files
    added ET files

---
+++ pages/close_processing.php
@@ -0,0 +1,95 @@
+<div style="margin:0 auto; width: 500px; margin-top: 30px">
+	<!-- <button type="button" class="btn btn-success" style="margin-left:0px">Save</button> -->
+	<div class="panel panel-primary clearfix">
+		<div class="panel-heading clearfix" style="height:50px">
+			<h3 class="panel-title pull-left">Close Reporting</h3>
+		</div>
+		<div style="margin:10px">
+			<div class="checkbox clearfix">
+				<div style="width: 275px;">
+					<label class"pull-left">
+			    		<input type="checkbox"> Attend close review meeting once a month with all the stakeholders
+			   		</label>
+			 	</div>
+				<div style="margin-top:-40px">
+			 		<form class="form-horizontal pull-right"role="form" style="width:100px;">
+			   			<div class="form-group">
+			    			<div class="col-sm-10">
+			      				<input type="text" class="form-control" id="inputTime" placeholder="Time">
+			    			</div>
+						</div>
+			 		</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="panel panel-primary">
+		<div class="panel-heading">
+			<h3 class="panel-title">Monitoring Close Tracks</h3>
+		</div>
+		<div style="margin:10px">
+			<div class="checkbox clearfix">
+				<div style="width: 275px;">
+					<label class"pull-left">
+			    		<input type="checkbox">Tracks 1-14
+			   		</label>
+			 	</div>
+				<div style="margin-top:-40px">
+			 		<form class="form-horizontal pull-right"role="form" style="width:100px;">
+			   			<div class="form-group">
+			    			<div class="col-sm-10">
+			      				<input type="text" class="form-control" id="inputTime" placeholder="Time">
+			    			</div>
+						</div>
+			 		</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="panel panel-primary">
+		<div class="panel-heading">
+			<h3 class="panel-title">Close Event Escalation</h3>
+		</div>
+		<div style="margin:10px">
+			<div class="checkbox clearfix">
+				<div style="width: 275px;">
+					<label class"pull-left">
+			    		<input type="checkbox"> OCC monitors all the events and opens Priority 1/2 incidents
+			   		</label>
+			 	</div>
+				<div style="margin-top:-40px">
+			 		<form class="form-horizontal pull-right"role="form" style="width:100px;">
+			   			<div class="form-group">
+			    			<div class="col-sm-10">
+			      				<input type="text" class="form-control" id="inputTime" placeholder="Time">
+			    			</div>
+						</div>
+			 		</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="panel panel-primary">
+		<div class="panel-heading">
+			<h3 class="panel-title">Close Review Meeting</h3>
+		</div>
+		<div style="margin:10px">
+			<div class="checkbox clearfix">
+				<div style="width: 275px;">
+					<label class"pull-left">
+			    		<input type="checkbox"> Attend close review meeting once a month with all the stake holders
+			   		</label>
+			 	</div>
+				<div style="margin-top:-40px">
+			 		<form class="form-horizontal pull-right"role="form" style="width:100px;">
+			   			<div class="form-group">
+			    			<div class="col-sm-10">
+			      				<input type="text" class="form-control" id="inputTime" placeholder="Time">
+			    			</div>
+						</div>
+			 		</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
